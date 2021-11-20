88cc1321ad3d8da020d2c74297c7a5c0b0b8b67b
antoineriche <riche.antoine.pro@gmail.com>
2021-11-20 18:10:38 +0100

    add cmd controller

---
+++ src/main/java/com/ariche/domotest/rest/CommandController.java
@@ -0,0 +1,51 @@
+package com.ariche.domotest.rest;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController("cmd")
+public class CommandController {
+
+    @Value("${jeedom.api.base-url}")
+    private String baseUrl;
+
+    @Value("${jeedom.api.context}")
+    private String context;
+
+    @Value("${jeedom.api.key}")
+    private String key;
+
+    @GetMapping("/light-on")
+    public String switchLightOnn() {
+        return postCommand(52);
+    }
+
+    @GetMapping("/light-off")
+    public String switchLightOff() {
+        return postCommand(53);
+    }
+
+    @GetMapping("/{commandId}")
+    public String applyCommand(@RequestParam("commandId") Integer commandId) {
+        return postCommand(commandId);
+    }
+
+    private String postCommand(final int commandId) {
+        return new RestTemplate().getForEntity(buildURL(commandId), String.class).getBody();
+    }
+
+    private String buildURL(final int commandId) {
+        return "http://"
+                .concat(baseUrl)
+                .concat("/")
+                .concat(context)
+                .concat("?apikey=")
+                .concat(key)
+                .concat("&type=cmd")
+                .concat("&id=")
+                .concat(String.valueOf(commandId));
+    }
+}
---
+++ src/main/java/com/ariche/domotest/rest/PingController.java
@@ -0,0 +1,15 @@
+package com.ariche.domotest.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController()
+public class PingController {
+
+
+    @GetMapping("/ping")
+    public String ping() {
+        return "pong";
+    }
+
+}
